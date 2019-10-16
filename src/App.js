b34fa50cf8f75ca1b4ff12afb1ba68c30b0b14dd
Benjamin Aplin <benjaminaplin@gmail.com>
2019-10-16 14:33:01 -0500

    add router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
-import React, { Component } from 'react';
+import React from "react";
 import './App.css';
-import ColorChanger from './components/colorChanger'
+import ColorChanger from './components/ColorChanger'
+import JavascriptLand from './components/JavascriptLand'
+import Home from './components/Home'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <ColorChanger />
-    )
-  }
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/color-changer">Color Changer</Link>
+            </li>
+            <li>
+              <Link to="/javascript-land">JavascriptLand</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/color-changer">
+            <ColorChanger />
+          </Route>
+          <Route path="/javascript-land">
+            <JavascriptLand />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App;
+
---
+++ src/components/JavascriptLand.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const JavascriptLand = () => <div>Welcome to Javscript Land</div>
+
+export default JavascriptLand
